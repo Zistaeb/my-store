1e788dc1d97a4352d28ba774c24ddd7ef52764ef
Betty <bperezatencio@gmail.com>
2023-09-06 11:57:00 -0300

    Ciclo de vida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Product } from './models/product.model'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   imgParent = '';
 
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,19 +1,42 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
   imageDefault = './../../../assets/images/foto.jpg';
 
-  constructor() {}
+  constructor() {
+    //before render
+    //NO correr cosas asyncronas en el constructor, ejemplo fetch --- esto corre solo una vez
+    console.log('constructor', 'imgValue =>', this.img);
+  }
+
+  ngOnChanges() {
+    //before y durante render
+    //su objetivo es estar actualizando los cambios en el input --- corre muchas veces
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+  }
 
   ngOnInit(): void {
+    //before render
+    //se puede correr cosas async -- fetch, llamadas API, promesas -- corre una sola vez
+    console.log('ngOnInit', 'imgValue =>', this.img);
+  }
 
+  ngAfterViewInit(): void {
+    // after render
+    //handler children
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy(): void {
+    //solo se corre cuando se vaya a eliminar ese compoenente
+    console.log('ngOnDestroy');
   }
 
   imgError() {
